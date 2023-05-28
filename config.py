9893c3ccd8a44e68c0f52ec2c73a477253857298
romanaop76 <132418005+romanaop76@users.noreply.github.com>
2023-05-28 07:34:57 +0530

    config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/romanaop76/music9")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TheLove2Forever")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TheLove2Forever")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BELLA_CLAB")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/BELLA_CLAB")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
